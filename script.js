c9ced84c9a3b8637f3d0371a705fc725153126a2
FreymanReyes <frerey31@gmail.com>
2025-04-07 18:45:53 -0500

    Trabajando en about y quitando del navbar Mi Portafolio.

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ window.onscroll = function() {
         navbar.classList.remove('scrolled');
     }
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const aboutSection = document.getElementById("about");
+    const title = aboutSection.querySelector(".section-title");
+    const text = aboutSection.querySelector(".about-text");
+  
+    function animateOnScroll() {
+      const rect = aboutSection.getBoundingClientRect();
+      if (rect.top <= window.innerHeight * 0.8) {
+        title.classList.add("animate-title");
+        text.classList.add("typing-effect");
+        window.removeEventListener("scroll", animateOnScroll);
+      }
+    }
+  
+    window.addEventListener("scroll", animateOnScroll);
+    animateOnScroll(); // por si ya está visible al cargar
+  });
